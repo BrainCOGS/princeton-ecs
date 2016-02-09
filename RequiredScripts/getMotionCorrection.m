74601aff5f391af1552f6bdeff23fc1375776561
Sue Ann Koay <sueannkoay@gmail.com>
2016-02-09 16:51:03 -0500

    Read multiple motion corrected movies.

--- RequiredScripts/getMotionCorrection.m
+++ RequiredScripts/getMotionCorrection.m
@@ -1,11 +1,24 @@
 %%
-function [frameCorr, fileCorr] = getMotionCorrection(inputFiles, recompute, varargin)
+function [frameCorr, fileCorr] = getMotionCorrection(inputFiles, recompute, globalRegistration, varargin)
   
+  % Default arguments
   if nargin < 2
     recompute   = false;
   end
   if nargin < 3
+    globalRegistration  = true;
+  end
+  if nargin < 4
     varargin    = {10, 1, false, 0.1};
+  end
+  forceLoad     = false;
+  if ischar(recompute)
+    if strcmpi(recompute, 'never')
+      forceLoad = true;
+      recompute = false;
+    else
+      error('getMotionCorrection:arguments', 'recompute must be either true, false, or the string ''never'' to require that motion correction info already exist.');
+    end
   end
   
 
@@ -17,6 +30,8 @@ function [frameCorr, fileCorr] = getMotionCorrection(inputFiles, recompute, vara
 
     if ~recompute && exist(corrPath, 'file')
       load(corrPath);
+    elseif forceLoad
+      error('getMotionCorrection:nodata', 'Motion correction information file "%s" not found.', corrPath);
     else
       if isFirst
         isFirst = false;
@@ -39,7 +54,7 @@ function [frameCorr, fileCorr] = getMotionCorrection(inputFiles, recompute, vara
   if numel(inputFiles) > 1
     refImage    = cat(3, frameCorr.reference);
     fileCorr    = cv.motionCorrect(refImage, varargin{1:min(4,end)});
-    if nargout < 2
+    if globalRegistration
       for iFile = 1:numel(inputFiles)
         frameCorr(iFile).xShifts(:,end+1) = frameCorr(iFile).xShifts(:,end) + fileCorr.xShifts(iFile, end);
         frameCorr(iFile).yShifts(:,end+1) = frameCorr(iFile).yShifts(:,end) + fileCorr.yShifts(iFile, end);
--- mex/+cv/imfinfox.m
+++ mex/+cv/imfinfox.m
@@ -1,10 +1,11 @@
 % IMFINFOX    Gets information about an image stack with minimal amount of frame reading.
-  The data structure returned by this function can be passed to subsequent
-  functions like cv.imreadx() to actually load the frames, obtain motion
-  correction shifts, and so forth.
+%
+%  The data structure returned by this function can be passed to subsequent
+%  functions like cv.imreadx() to actually load the frames, obtain motion
+%  correction shifts, and so forth.
 %
 %  Usage syntax:
-%    info = imfinfox( inputPath, [maxNumFrames = inf], [blackTolerance = nan] );
+%    info = imfinfox( inputPath );
 %  where inputPath can be either a string (single file), or a cellstring for 
 %  multi-file stacks.
 %
