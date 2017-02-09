51c5c2f46faad120d7c002e7a3abfdcbb2413380
Sue Ann Koay <koay@princeton.edu>
2017-02-09 07:06:21 -0500

    Need to use barycentric coordinates in generative mode.

--- mex/+ecs/imreadnonlin.m
+++ mex/+ecs/imreadnonlin.m
@@ -3,7 +3,8 @@ function movie = imreadnonlin( inputPath, mcorr, gridUpsample )
   
   %% Default arguments
   if nargin < 3
-    gridUpsample        = [4 4];
+%     gridUpsample        = [4 4];
+    gridUpsample        = [2 2];
   elseif numel(gridUpsample) < 2
     gridUpsample        = [gridUpsample gridUpsample];
   end
@@ -20,6 +21,8 @@ function movie = imreadnonlin( inputPath, mcorr, gridUpsample )
   yCenter               = [1, round(linspace(mcorr.yCenter(1), mcorr.yCenter(end), gridSize(1))), nRows]';
   patchNx               = diff(xCenter);
   patchNy               = diff(yCenter);
+  patchXidx             = arrayfun(@(x,y) x + (0:y), xCenter(1:end-1), patchNx, 'UniformOutput', false);
+  patchYidx             = arrayfun(@(x,y) x + (0:y), yCenter(1:end-1), patchNy, 'UniformOutput', false);
   
   
   %% Read input movie
@@ -30,59 +33,73 @@ function movie = imreadnonlin( inputPath, mcorr, gridUpsample )
     movie               = cv.imtranslatex(movie, mcorr.rigid.xShifts(:,end), mcorr.rigid.yShifts(:,end));
   end
   
+  
   %%
 %   profile on
   tic
-  corrected             = cell(1,1,nFrames);
+  corrected             = nan(size(movie), 'like', movie);
+  nPixels               = nRows * nCols;
   for iFrame = 1:nFrames
     %%
-    xLoc                = bsxfun(@plus, patchXShifts(:,:,iFrame), xCenter);
-    yLoc                = bsxfun(@plus, patchYShifts(:,:,iFrame), yCenter);
+    xLoc                = double(bsxfun(@plus, patchXShifts(:,:,iFrame), xCenter));
+    yLoc                = double(bsxfun(@plus, patchYShifts(:,:,iFrame), yCenter));
 %     idx                 = knnsearch([yLoc(:),xLoc(:)], queryGrid);
 
     %% 
     for iX = 1:numel(xCenter)-1
       for iY = 1:numel(yCenter)-1
         %% Bounding coordinate rectangle
-        patchX          = xLoc(iX:iX+1,iY:iY+1);
-        patchY          = yLoc(iX:iX+1,iY:iY+1);
+        patchX          = xLoc(iY:iY+1,iX:iX+1);
+        patchY          = yLoc(iY:iY+1,iX:iX+1);
         boundX          = [ceil(min(patchX(:))), floor(max(patchX(:)))];
         boundY          = [ceil(min(patchY(:))), floor(max(patchY(:)))];
         
         %%
         x21             = patchX(:,2) - patchX(:,1);
-        x21             = (x21(1) + x21(2)) / 2 / patchNx(iX);
         y21             = patchY(:,2) - patchY(:,1);
-        y21             = (y21(1) + y21(2)) / 2 / patchNy(iY);
         x31             = patchX(2,:) - patchX(1,:);
-        x31             = (x31(1) + x31(2)) / 2 / patchNx(iX);
         y31             = patchY(2,:) - patchY(1,:);
-        y31             = (y31(1) + y31(2)) / 2 / patchNy(iY);
+%         x21             = (x21(1) + x21(2)) / 2;
+%         y21             = (y21(1) + y21(2)) / 2;
+%         x31             = (x31(1) + x31(2)) / 2;
+%         y31             = (y31(1) + y31(2)) / 2;
+        x21             = max(x21(1), x21(2));
+        y21             = max(y21(1), y21(2));
+        x31             = max(x31(1), x31(2));
+        y31             = max(y31(1), y31(2));
+        
+        norm21          = x21.^2 + y21.^2;
+        norm31          = x31.^2 + y31.^2;
         
         %% All points inside rectangle
-        Px              = boundX(1):boundX(2);
-        Py              = boundY(1):boundY(2);
-        Px              = Px - ( patchX(1,1) + x21*(0:patchNx(iX)-1) );
-        Py              = Py - ( patchY(1,1) + y31*(0:patchNy(iX)-1) );
-        
-%         [Px,Py]         = meshgrid(boundX(1):boundX(2), boundY(1):boundY(2));
+        [outX,outY]     = meshgrid(boundX(1):boundX(2), boundY(1):boundY(2));
+        Px              = outX - patchX(1);
+        Py              = outY - patchY(1);
+        proj21          = ( Px*x21 + Py*y21 ) / norm21;
+        proj31          = ( Px*x31 + Py*y31 ) / norm31;
+        inside          = proj21 >= 0 & proj21 <= 1 & proj31 >= 0 & proj31 <= 1;
+        proj21          = proj21(inside) * patchNx(iX);
+        proj31          = proj31(inside) * patchNy(iY);
+        i21             = floor(proj21);
+        i31             = floor(proj31);
+        w21             = proj21 - i21;
+        w31             = proj31 - i31;
         
         %%
-%         [x21, x31; y21, y31]
-       
+        rows            = yCenter(iY) + i31;
+        cols            = xCenter(iX) + i21;
+        oFrame          = nPixels*(iFrame-1);
+        interpolated    = (1 - w21).*(1 - w31).*movie(rows   + nRows*(cols-1) + oFrame)   ...
+                        +      w21 .*(1 - w31).*movie(rows   + nRows*(cols  ) + oFrame)   ...
+                        + (1 - w21).*     w31 .*movie(rows+1 + nRows*(cols-1) + oFrame)   ...
+                        +      w21 .*     w31 .*movie(rows+1 + nRows*(cols  ) + oFrame)   ...
+                        ;
+                      
+        %%
+        index           = outY(inside) + nRows*(outX(inside) - 1) + oFrame;
+        corrected(index)= interpolated;
       end
     end
-
-    
-    %%
-    %{
-    for iRow = 1:nRows
-      for iCol = 1:nCols-1
-        a = movie(iRow,iCol,iFrame);
-        a = movie(iRow,iCol+1,iFrame);
-      end
-    end
-    %}
   end
   toc
 %   profile viewer
