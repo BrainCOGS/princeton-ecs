4051ca78171f4e8376c5e805ef8c2e68eb539785
Sue Ann Koay <koay@princeton.edu>
2017-08-15 10:25:41 -0400

    Option to create backup file

--- scripts/parallelSave.m
+++ scripts/parallelSave.m
@@ -1,6 +1,23 @@
 %% save() functionality for parallel computing. N.B. All arguments must be Matlab variables, e.g. an array element x(i) will break this code!
-function parsave(filename, varargin)
+function parallelSave(filename, varargin)
 
+  %% Make a backup if the target exists
+  if iscell(filename)
+    backupFile        = filename{2};
+    filename          = filename{1};
+    
+    if exist(filename, 'file')
+      if exist(backupFile, 'file')
+        delete(backupFile);
+      end
+      [status,msg]    = movefile(filename, backupFile);
+      if ~status
+        error('parallelSave:backupFile', msg);
+      end
+    end
+  end
+  
+  %% Save all specified variables with their names as passed to this function
   options             = {};
   what                = struct();
   for iArg = 1:numel(varargin)
